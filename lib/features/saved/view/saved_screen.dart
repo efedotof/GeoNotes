398236b0c2c279476444aa93ed2e4da659aff5ae
Egor <80205178+efedotof@users.noreply.github.com>
2024-09-07 11:21:45 +0300

    Update saved_screen.dart

--- lib/features/saved/view/saved_screen.dart
+++ lib/features/saved/view/saved_screen.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'package:flutter/material.dart';
 import 'package:auto_route/auto_route.dart';
 
@@ -9,13 +8,13 @@ class SavedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DraggableScrollableSheet(
-      initialChildSize: 0.3,  // Начальный размер экрана (30% высоты)
-      minChildSize: 0.1,      // Минимальный размер экрана (10% высоты)
-      maxChildSize: 0.8,      // Максимальный размер экрана (80% высоты)
+      initialChildSize: 0.3,  
+      minChildSize: 0.1,      
+      maxChildSize: 0.8,      
       builder: (BuildContext context, ScrollController scrollController) {
         return GestureDetector(
           onTap: () {
-            // Закрыть все страницы и вернуться к корневой
+       
             AutoRouter.of(context).popUntilRoot();
           },
           child: Container(
@@ -32,7 +31,7 @@ class SavedScreen extends StatelessWidget {
                 Center(
                   child: Text('Saved'),
                 ),
-                // Добавьте другие виджеты по необходимости
+
               ],
             ),
           ),
@@ -41,47 +40,3 @@ class SavedScreen extends StatelessWidget {
     );
   }
 }
-=======
-import 'package:flutter/material.dart';
-import 'package:auto_route/auto_route.dart';
-
-@RoutePage()
-class SavedScreen extends StatelessWidget {
-  const SavedScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return DraggableScrollableSheet(
-      initialChildSize: 0.3,  // Начальный размер экрана (30% высоты)
-      minChildSize: 0.1,      // Минимальный размер экрана (10% высоты)
-      maxChildSize: 0.8,      // Максимальный размер экрана (80% высоты)
-      builder: (BuildContext context, ScrollController scrollController) {
-        return GestureDetector(
-          onTap: () {
-            // Закрыть все страницы и вернуться к корневой
-            AutoRouter.of(context).popUntilRoot();
-          },
-          child: Container(
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topRight: Radius.circular(14),
-                topLeft: Radius.circular(14),
-              ),
-              color: Color(0xFFF7F6F2),
-            ),
-            child: ListView(
-              controller: scrollController,
-              children: const [
-                Center(
-                  child: Text('Saved'),
-                ),
-                // Добавьте другие виджеты по необходимости
-              ],
-            ),
-          ),
-        );
-      },
-    );
-  }
-}
->>>>>>> 2b05aa3260d4acb858d542f827af24c877766ba5
