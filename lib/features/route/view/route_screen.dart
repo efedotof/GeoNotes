c9df0525583fc2fd34b7da7c884c6cfb697294b2
efedotov <fedmehol@icloud.com>
2024-09-07 18:25:33 +0300

    updating the  on the route page

--- lib/features/route/view/route_screen.dart
+++ lib/features/route/view/route_screen.dart
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> 7004069dcc966194bb1c81015e376a046fa04b45
 import 'package:flutter/material.dart';
 import 'package:auto_route/auto_route.dart';
 
@@ -12,9 +8,9 @@ class RouteScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DraggableScrollableSheet(
-      initialChildSize: 0.3,  
-      minChildSize: 0.1,      
-      maxChildSize: 0.8,      
+      initialChildSize: 0.3,  // Adjusted to the height you may want
+      minChildSize: 0.1,      // Minimum drag extent
+      maxChildSize: 0.8,      // Maximum drag extent
       builder: (BuildContext context, ScrollController scrollController) {
         return GestureDetector(
           onTap: () {
@@ -26,16 +22,105 @@ class RouteScreen extends StatelessWidget {
                 topRight: Radius.circular(14),
                 topLeft: Radius.circular(14),
               ),
-              color: Color(0xFFF7F6F2),
+              color: Color(0xFFF7F6F2), // Off-white background color
             ),
             child: ListView(
               controller: scrollController,
-              children: const [
-                Center(child: Text('Route')),
-<<<<<<< HEAD
-                // Добавьте другие виджеты по необходимости
-=======
->>>>>>> 7004069dcc966194bb1c81015e376a046fa04b45
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+                  child: Column(
+                    children: [
+                    
+                      const SizedBox(height: 8),
+                      // Title and icons row
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            width: MediaQuery.of(context).size.width * 0.5,
+                            height: MediaQuery.of(context).size.height * 0.09,
+                         
+                            decoration: BoxDecoration(
+                              color: Colors.grey[200],
+                              borderRadius: BorderRadius.circular(14)
+                            ),
+                            child: const Center(
+                              child: ListTile(
+                                leading: Icon(Icons.directions_walk, color: Colors.grey),
+                                title: Text(
+                                '1 hr, 05 min',
+                                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                                
+                              ),
+                              subtitle:  Text(
+                                '6 km',
+                                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.grey),
+                              
+                              )),
+                            ),
+                          ),
+                          const Wrap(
+                            spacing: 14,
+                            children: [
+                              Icon(Icons.directions_car, color: Colors.grey),
+                              Icon(Icons.train, color: Colors.grey),
+                              Icon(Icons.directions_walk, color: Colors.grey),
+                              Icon(Icons.pedal_bike, color: Colors.grey),
+                            ],
+                          )
+                        ],
+                      ),
+                      const SizedBox(height: 12),
+                      // Locations
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                              borderRadius: BorderRadius.circular(14)
+                        ),
+                        child: ListTile(
+                          tileColor:Colors.grey[200],
+                          leading: Icon(Icons.location_pin, color: Theme.of(context).primaryColor),
+                          title: const Text('My location'),
+                          trailing:const Text(
+                                'Map',
+                                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.grey),
+                              
+                             
+                          ),
+                          onTap: () {},
+                        ),
+                      ),
+                      const SizedBox(height: 10,),
+                      Container(
+                         decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                              borderRadius: BorderRadius.circular(14)
+                        ),
+                        child: ListTile(
+                          leading: Icon(Icons.restaurant, color: Theme.of(context).primaryColor),
+                          title: const Text("Restaurant Zaza's"),
+                          trailing:  const Text(
+                                'Map',
+                                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.grey),
+                              
+                             
+                          ),
+                          onTap: () {}, 
+                        ),
+                      ),
+                      const SizedBox(height: 12),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: const Text('Start', style: TextStyle (color: Colors.white),),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xFF102930),
+                          minimumSize: const Size.fromHeight(50), // Button height
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
@@ -44,8 +129,3 @@ class RouteScreen extends StatelessWidget {
     );
   }
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> 2b05aa3260d4acb858d542f827af24c877766ba5
->>>>>>> 7004069dcc966194bb1c81015e376a046fa04b45
